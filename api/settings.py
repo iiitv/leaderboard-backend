e81baadb93357096060a5baacd13ade5b86218c8
mugdha273 <mugdhasharma0327@gmail.com>
2022-09-19 18:34:54 +0530

    app setup

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-7ce=z(%g5i9+evv%5zoi8t%=wsigv%vu)rdk^gk=prfkwnf$j@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "leaderboard"
 ]
 
 MIDDLEWARE = [
