0689ce14413d9c00483468c8ee16ef6a9932bbf3
mugdha273 <mugdhasharma0327@gmail.com>
2022-09-20 15:13:03 +0530

    views change

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -10,14 +10,5 @@ from ipaddress import ip_address, ip_network
 @csrf_exempt
 def hello(request):
     # Verify if request came from GitHub
-    forwarded_for = u'{}'.format(request.META.get('HTTP_X_FORWARDED_FOR'))
-    client_ip_address = ip_address(forwarded_for)
-    whitelist = requests.get('https://api.github.com/meta').json()['hooks']
-
-    for valid_ip in whitelist:
-        if client_ip_address in ip_network(valid_ip):
-            break
-    else:
-        return HttpResponseForbidden('Permission denied.')
 
     return HttpResponse('pong')
