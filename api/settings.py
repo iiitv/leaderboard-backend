0cfbcd1a6e3b43845fb048f90d36f87c26bd12ee
Phoenix <202051144@iiitvadodara.ac.in>
2022-10-03 19:52:56 +0530

    changed to default .env

--- api/settings.py
+++ api/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 from django.test.runner import DiscoverRunner
 from dotenv import load_dotenv
 
-load_dotenv('.env.dev')
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
